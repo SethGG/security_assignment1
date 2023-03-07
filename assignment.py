974927bd2012bdd41ce797a9902759f717c90a28
dzee <df.zee@hotmail.com>
2023-03-07 22:56:38 +0100

    comments

--- assignment.py
+++ assignment.py
@@ -4,12 +4,20 @@ import zlib
 import shutil
 import sys
 
-verbose = sys.argv[1] == "-v" if len(sys.argv) > 1 else False
-password_txt_path = glob.glob("*.txt")[0]
-working_directory = ""
+# Security 2023 Assignment 1
+# Daniël Zee s2063131
 
 
 def bruteforce_archive(zip_path, txt_path):
+    """ Try all the passwords in a txt file to extract the content of a zip file to a folder with the same name.
+
+    Parameters:
+        zip_path (str): The path of the zip file to be extracted.
+        txt_path (str): The path of the txt file where the passwords are taken from.
+
+    Returns:
+        (str): The path of the folder where the contents of the zip file were extracted to.
+    """
     with zipfile.ZipFile(zip_path, mode="r") as archive:
         with open(txt_path, mode="rb") as txt_file:
             print(f"Archive: {zip_path}\nTrying all passwords in {txt_path}") if verbose else None
@@ -25,11 +33,14 @@ def bruteforce_archive(zip_path, txt_path):
             raise RuntimeError(f"None of the passwords in {txt_path} were correct for {zip_path}.")
 
 
+verbose = sys.argv[1] == "-v" if len(sys.argv) > 1 else False  # User has the option to show the progress of the script
+password_txt_path = glob.glob("*.txt")[0]  # The first txt file the script finds is used for the passwords
+working_directory = ""
 archives = glob.glob(working_directory + "*.zip")
-while archives:
+while archives:  # Continue bruteforcing until the extracted folder does not contain a zip file
     working_directory = bruteforce_archive(archives[0], password_txt_path)
     archives = glob.glob(working_directory + "*.zip")
-else:
+else:  # Print the content of all the found txt files to the screen and move the files to the root folder
     for txt_path in glob.glob(working_directory + "*.txt"):
         with open(txt_path, mode="r") as t:
             print(t.read())
