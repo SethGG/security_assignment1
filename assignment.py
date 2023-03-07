b985eb2056fa15944107b4d6beb1e42875da128c
dzee <df.zee@hotmail.com>
2023-03-07 19:49:46 +0100

    verbose argument

--- assignment.py
+++ assignment.py
@@ -2,7 +2,9 @@ import zipfile
 import glob
 import zlib
 import shutil
+import sys
 
+verbose = sys.argv[1] == "-v" if len(sys.argv) > 1 else False
 password_txt_path = glob.glob("*.txt")[0]
 working_directory = ""
 
@@ -10,12 +12,12 @@ working_directory = ""
 def bruteforce_archive(zip_path, txt_path):
     with zipfile.ZipFile(zip_path, mode="r") as archive:
         with open(txt_path, mode="rb") as txt_file:
-            print(f"Archive: {zip_path}\nTrying all passwords")
+            print(f"Archive: {zip_path}\nTrying all passwords in {txt_path}") if verbose else None
             for line in txt_file:
                 password = line.strip()
                 try:
                     archive.extractall(zip_path[:-4], pwd=password)
-                    print(f"Password: {password} was correct!\n")
+                    print(f"Password: {password} was correct!\n") if verbose else None
                     return zip_path[:-4] + "/"
                 except (RuntimeError, zlib.error, zipfile.BadZipFile):
                     continue
@@ -23,7 +25,6 @@ def bruteforce_archive(zip_path, txt_path):
             raise RuntimeError(f"None of the passwords in {txt_path} were correct for {zip_path}.")
 
 
-print(f"Password file: {password_txt_path}\n")
 archives = glob.glob(working_directory + "*.zip")
 while archives:
     working_directory = bruteforce_archive(archives[0], password_txt_path)
